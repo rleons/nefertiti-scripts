4aff1415da80706c1839d90d4872502bc4da5dba
Roberto Leon <13802215+rleons@users.noreply.github.com>
2021-06-03 18:32:00 -0400

    Create binance-cancel-orders.sh

---
+++ Binance/binance-cancel-orders.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Aurhor: Roberto Leon
+# Description: Nefertiti Crypto Trading Bot - Cancell all unfilled orders
+# Example: ./*-cancel-orders.sh --side=buy
+
+### Keys
+apikey="x"
+apisec="x"
+telekey="x"
+telechat="x"
+pushapp="x"
+pushkey="x"
+
+### Flag args
+for i in "$@"
+do
+case $i in
+  --side=*)
+  side="${i#*=}"
+  shift;;
+esac
+done
+if [ -z ${side+x} ]; then
+  echo $(date +"%Y/%m/%d %H:%M:%S") "[ERROR] missing argument: --side"
+  exit 1
+fi
+
+### Settings
+exchange="BINA"
+markets=$(cat markets.now)
+
+### Build params string
+params="
+--exchange=$exchange \
+--side=$side
+--api-key=$apikey \
+--api-secret=$apisec \
+--api-passphrase=$apipass \
+--telegram-app-key=$telekey \
+--telegram-chat-id=$telechat \
+--pushover-app-key=$pushapp \
+--pushover-user-key=$pushkey"
+
+### Execute Nefertiti
+echo $(date +"%Y/%m/%d %H:%M:%S") "[INFO] Cancelling $side orders..."
+for market in ${markets//,/ }; do
+  cryptotrader cancel --market=$market $params
+  echo $(date +"%Y/%m/%d %H:%M:%S") "[INFO] All $market buy orders have been cancelled."
+done
+echo $(date +"%Y/%m/%d %H:%M:%S") "[INFO] Done."
